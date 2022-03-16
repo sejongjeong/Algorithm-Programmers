1d2787562039f8827e71e0b0aa13f92f71aa3ddd
sejongjeong <wjdtpwhd@gmail.com>
2022-03-17 00:07:58 +0900

    add solution: 2022_kakao_blind_4

--- src/2022 KAKAO BLIND RECRUITMENT/양궁대회.py
+++ src/2022 KAKAO BLIND RECRUITMENT/양궁대회.py
@@ -44,3 +44,42 @@ def solution(n, info):
     if max_score == 0:
         answer = [-1]
     return answer
+
+
+#0점으로 가까운 점수가 가장 많게
+# 백트래킹
+def solution(n, info):
+    def cal_score(a_list, l_list): #return 최종 점수 차이
+        #라이언이 이기면 +, 어피치가 이기면 -
+        l_score, a_score = 0, 0
+        for i in range(11):
+            if a_list[i] == l_list[i] == 0:
+                continue
+            elif a_list[i] >= l_list[i]:
+                a_score += i
+            elif a_list[i] < l_list[i]:
+                l_score += i
+        return l_score - a_score
+    
+    max_score = 0
+    tmp_list = []
+    answer = []
+    
+    def recursion(left, layer):
+        nonlocal max_score, tmp_list, answer
+        if layer == 11 and left == 0:
+            tmp = cal_score(info[::-1], tmp_list)
+            if max_score < tmp :
+                max_score = tmp
+                answer = tmp_list[::-1]
+            return
+        elif layer == 11:
+            return
+        for i in range(left, -1, -1):
+            tmp_list.append(i)
+            recursion(left - i, layer + 1)
+            tmp_list.pop()
+    recursion(n, 0)
+    if max_score == 0:
+        answer = [-1]
+    return answer
