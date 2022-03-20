dc2cc4a84924eef376f8200b898cd8829dbeb2d4
sejongjeong <wjdtpwhd@gmail.com>
2022-03-21 01:05:01 +0900

    add solution: 2021_kakao_internship_1

--- src/2021_KAKAO_INTERNSHIP/숫자_문자열과_영단어.py
+++ src/2021_KAKAO_INTERNSHIP/숫자_문자열과_영단어.py
@@ -1,14 +1,29 @@
 def solution(s):
-    answer = ''
-    check = False
-    num_list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    tmp = ''
-    for i,c in enumerate(list(s)):
-        if c.isdigit():
-            answer += c
-            continue
-        tmp += c
-        if tmp in num_list:
-            answer += str(num_list.index(tmp))
-            tmp = ''
-    return int(answer)
+	answer = ''
+	num_list = ['zero', 'one', 'two', 'three', 'four',
+				'five', 'six', 'seven', 'eight', 'nine']
+	tmp = ''
+	for i, c in enumerate(list(s)):
+		if c.isdigit():
+			answer += c
+			continue
+		tmp += c
+		if tmp in num_list:
+			answer += str(num_list.index(tmp))
+			tmp = ''
+	return int(answer)
+
+def solution(s):
+	answer = ''
+	num_dic = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4,
+			   'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+	tmp = ''
+	for i, c in enumerate(list(s)):
+		if c.isdigit():
+			answer += c
+			continue
+		tmp += c
+		if tmp in num_dic:
+			answer += str(num_dic[tmp])
+			tmp = ''
+	return int(answer)
