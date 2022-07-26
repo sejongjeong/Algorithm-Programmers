4a5732d1585a6072d4c6154c9e83c1b18f067cb4
Sejong <wjdtpwhd@gmail.com>
2022-07-26 23:22:33 +0900

    solved: 2022_kakao_blind_5

--- src/2022 KAKAO BLIND RECRUITMENT/양과_늑대.py
+++ src/2022 KAKAO BLIND RECRUITMENT/양과_늑대.py
@@ -1,4 +1,28 @@
+from collections import defaultdict
+
 def solution(info, edges):
-    answer = 0
-    tmp = []
+    eg = defaultdict(list)
+    for p, c in edges:
+        eg[p].append(c)
+    
+    def dfs(sheep, wolf, current, path):
+        if info[current]:
+            wolf+=1
+        else:
+            sheep += 1
+        if sheep <= wolf:
+            return 0
+        
+        maxSheep = sheep
+        
+        for p in path:
+            for n in eg[p]:
+                if n not in path:
+                    path.append(n)
+                    print(n, path)
+                    maxSheep = max(maxSheep, dfs(sheep, wolf, n,path))
+                    path.pop()
+        return maxSheep
+    
+    answer = dfs(0,0,0,[0])
     return answer
